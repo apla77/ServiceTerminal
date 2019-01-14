c78526c8b41f828caab12ee6ae91671d8406d5b0
apla77 <verocidade77@gmail.com>
2019-01-14 10:17:42 -0300

    commite 15/01/2019-10:10h

--- app/controllers/Terminais.java
+++ app/controllers/Terminais.java
@@ -20,13 +20,16 @@ public class Terminais extends Controller {
 	// Consumido pela Terminal 
 	public static void setIpterminal(String ipTerminal) {
 		IpTerminal ipTerminais = IpTerminal.find("ipTerminal = ?", ipTerminal).first();
+		System.out.println(" 1 ************************* ipTerminal = " + ipTerminal);
 		if(ipTerminais == null) {
+			System.out.println("No if setIpterminal");
 			IpTerminal ip = new IpTerminal();
 			ip.ipTerminal = ipTerminal;
 			ip.save();
 			ok();
 		}
 		else {
+			System.out.println("nada 1");
 			notFound();
 		}	
 	}
@@ -34,35 +37,46 @@ public class Terminais extends Controller {
 	// Consumido pela Urna
 	public static void finalizarVotacaoAtual(String status, String ipTerminal) { 
 		IpTerminal ipTerminais = IpTerminal.find("ipTerminal = ?", ipTerminal).first();
+		System.out.println(" 2 ************************* ipTerminal = " + ipTerminal);
 		
 		if(status.equals("finalizado") && ipTerminais != null) {
+			System.out.println("No if finalizarVotacaoAtual");
 			fimVoto = true;
 			ok();
 		}else {
+			System.out.println("nada 2");
 			fimVoto = false;
 			ok();
 		}
-	}
+	} 
 	
 	// Consumido pela Terminal
 	public static void confirmarVotacaoAtual(String ipTerminal) {
 		IpTerminal ipTerminais = IpTerminal.find("ipTerminal = ?", ipTerminal).first();
+		System.out.println(" 3 ************************* ipTerminal = " + ipTerminal);
 		if(fimVoto == true && ipTerminais != null) {
+			System.out.println("No if confirmarVotacaoAtual");
 			renderJSON(true);
 		}
-		else
+		else {
+			System.out.println("nada 3");
 			renderJSON(false);
+		}
+			
 	}
 	
 	// Consumido pela Urna
     public static void tempoParaUrna(Long codUrna, String ipTerminal){	
     	IpTerminal ipTerminais = IpTerminal.find("ipTerminal = ?", ipTerminal).first();	
+    	System.out.println(" 4 ************************* ipTerminal = " + ipTerminal);
    		if(ipTerminais != null) 
    		{   	
+   			System.out.println("No if tempoParaUrna");
    	   	    pedidoTempo = true;
    	   	    ok();
    		}
    		else{
+   			System.out.println("nada 4");
    			pedidoTempo = false; 
    			ok();
    		}	
@@ -71,10 +85,13 @@ public class Terminais extends Controller {
     // Consumido pela Terminal
     public static void addTempo(String ipTerminal){
     	 IpTerminal ipTerminais = IpTerminal.find("ipTerminal = ?", ipTerminal).first();	
+    	 System.out.println(" 5 ************************* ipTerminal = " + ipTerminal);
     	 if(pedidoTempo == true && ipTerminais != null) {
+    		 System.out.println("No if addTempo");
     		  renderJSON(true);  
     	  }
     	 else {
+    		 System.out.println("nada 5");
     		 renderJSON(false);
     	 }
       }    
